cf85727613cdc6ffeb53f4c80c9d8e182357d4d1
MelBou <bouzon.melissa@gmail.com>
2019-01-09 10:59:27 +0100

    tests added

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,4 +13,28 @@ class GildedRoseTest {
         app.updateQuality();
         assertThat(app.items[0].name).isEqualTo("foo");
     }
+
+    @Test
+    void wine1() {
+        Item[] items = new Item[] { new Item("Aging Red Wine 4", -150, 15) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(14);
+    }
+
+    @Test
+    void wine2() {
+        Item[] items = new Item[] { new Item("Aging Red Wine 2", -50, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(21);
+    }
+
+    @Test
+    void wine3() {
+        Item[] items = new Item[] { new Item("Aging Red Wine 2", 50, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(20);
+    }
 }
